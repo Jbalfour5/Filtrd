0e718d6839ccb5c2a7d2b83f1025b5999e8c1cd6
cheangdominic <dcheang@my.bcit.ca>
2025-12-01 12:39:17 -0800

    Added Filtrd logo and changed the browser title to accurately match the name of the app. Changed text on frontend to match accuracy of gameplay

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React from "react";
+import FiltrdLogo from "../assets/Filtrd.png";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between mb-4">
-      <div>
-        <h1 className="text-2xl font-bold tracking-tight">
-          FILTRD — Daily Filtered Clip
-        </h1>
-        <p className="text-sm text-gray-600">
-          Guess the song as filters are removed.
-        </p>
+      <div className="flex items-center gap-1">
+        <img
+          src={FiltrdLogo}
+          alt="Filtrd logo"
+          className="w-14 h-14 object-contain"
+        />
+
+        <div>
+          <h1 className="text-2xl font-bold tracking-tight">
+            FILTRD - Daily Filtered Clip
+          </h1>
+          <p className="text-sm text-gray-600">
+            Guess the song as filters are removed.
+          </p>
+        </div>
       </div>
 
+      {/* Right side button */}
       <button
         className="text-sm px-3 py-2 cursor-pointer rounded-md bg-gradient-to-br from-indigo-400 to-indigo-600 hover:to-indigo-800 text-white transition-colors"
         onClick={() => window.location.reload()}
